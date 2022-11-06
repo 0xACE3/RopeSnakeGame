af2c6b59e6a4342a37e26ba82deb39acbf2777e9
0xHaris <0xbitshot@gmail.com>
2022-11-07 02:31:35 +0500

    added requirements.txt and setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='RopeSnakeGame',
+    version='1.0',
+    packages=['rgbprint'],
+    url='https://github.com/0xHaris/RopeSnakeGame',
+    license='MIT',
+    author='0xharis',
+    author_email='0xbitshot@gmail.com',
+    description='This is N Players Rope Snake Game.'
+)
